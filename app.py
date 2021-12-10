2366ddf3067b701b3f9fbf36f9a50f5407ffc779
EKaizen2 <55081970+EKaizen2@users.noreply.github.com>
2021-12-10 23:45:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -16,36 +16,36 @@ st.header("This web app predicts comments inputted about the Ghanaina government
 
 comment =  st.text_input("write your comment about the ghanaian govenrment recent passed budget").lower()
 
-uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
-st.markdown(f"my input is: {comment}")
+# uploaded_file = st.file_uploader("Choose a XLSX file", type="xlsx")
+# st.markdown(f"my input is: {comment}")
 
-def predict_file():
-    model = pickle.load(open('sentiment_model','rb'))
-    # front end elements of the web page
-    html_temp = """ 
-    <div style ="background-color:#FF0000;padding:10px;font-weight:10px"> 
-    <h1 style ="color:white;>Ephraim Adongo Sport Prediction</h1> 
-    </div> 
-    """
+# def predict_file():
+#     model = pickle.load(open('sentiment_model','rb'))
+#     # front end elements of the web page
+#     html_temp = """ 
+#     <div style ="background-color:#FF0000;padding:10px;font-weight:10px"> 
+#     <h1 style ="color:white;>Ephraim Adongo Sport Prediction</h1> 
+#     </div> 
+#     """
 
-    # display the front end aspect
-    st.markdown(html_temp, unsafe_allow_html=True)
-    default_value_goes_here = ""
+#     # display the front end aspect
+#     st.markdown(html_temp, unsafe_allow_html=True)
+#     default_value_goes_here = ""
 
-    global dataframe
-    if uploaded_file:
-        df = pd.read_excel(uploaded_file)
-        dataframe = df
+#     global dataframe
+#     if uploaded_file:
+#         df = pd.read_excel(uploaded_file)
+#         dataframe = df
     
-    bow_vectorizer = CountVectorizer(max_df=9000, min_df=10, max_features=513, stop_words='english')
-    #comment_data = [str (item) for items in dataframe]
-    answer = bow_vectorizer.fit_transform(dataframe)
+#     bow_vectorizer = CountVectorizer(max_df=9000, min_df=10, max_features=513, stop_words='english')
+#     #comment_data = [str (item) for items in dataframe]
+#     answer = bow_vectorizer.fit_transform(dataframe)
 
-    result = ""  
+#     result = ""  
 
-    pred = model.predict(answer)
-    result = pred
-    st.write(result)
+#     pred = model.predict(answer)
+#     result = pred
+#     st.write(result)
 
 if st.button('Predict Comment'):
 	model = pickle.load(open('sentiment_model','rb'))
@@ -63,7 +63,7 @@ if st.button('Predict Comment'):
 	st.header("Please find predicted value below")
 	st.write("Your comment is predicted to be a ", prediction , " comment")
 	
-elif st.button('Predict uploaded data'):
-	predict_file()
+# elif st.button('Predict uploaded data'):
+# 	predict_file()
 else:
-	st.write("Try again")
+	st.write("WELCOME TO EPHRAIMS SENTIMENT ANALYSIS MODEL")
